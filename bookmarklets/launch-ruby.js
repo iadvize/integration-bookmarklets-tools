b258c49a1d0c43976cc81a800ba6223f961c8442
Fx C. <mr.eroak@gmail.com>
2026-04-09 16:11:25 +0200

    Update launch-ruby.js

--- bookmarklets/launch-ruby.js
+++ bookmarklets/launch-ruby.js
@@ -5,7 +5,12 @@
     lang: 'fr',
     sid: '7316'
   };
-  window.iAdvizeInterface.push((iAdvize) => iAdvize.navigate('ruby'));
+  window.iAdvizeInterface.push((iAdvize) => {
+    iAdvize.navigate('_blank');
+    setTimeout(function() {
+      iAdvize.navigate('ruby');
+    }, 1000);
+  });
   var script = document.createElement('script');
   script.src = 'https://halc.iadvize.com/iadvize.js';
   document.body.appendChild(script)
